61fd5d1fa31128901ce9be2c4e6813f82f5dce6a
JustG <guimachado94@gmail.com>
2023-01-24 16:06:37 -0300

    require image

--- src/components/produto.jsx
+++ src/components/produto.jsx
@@ -4,11 +4,12 @@ import no_image from "./img/no-image.png"
 import "./style/produto.css"
 
 const Produto = ({produto}) => {
+    let image = require(produto.img)
 
     return ( 
         <>  
             <div className="card">
-                <img src={produto.img || no_image} alt="Imagem do produto" />
+                <img src={image || no_image} alt="Imagem do produto" />
 
                 <p>{produto.nome}</p>
             </div>
