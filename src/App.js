9b0e1fc3926ef4e9b1b1b01f554b04185cbc0df0
JustG <guimachado94@gmail.com>
2023-01-24 16:47:44 -0300

    public images

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     },
     {
       cod: 3,
-      img: "./images/no-image.png",
+      img: "./images/trakinas.jpg",
       nome: "Bol. Recheada Trakinas choc preto",
       categoria: "Bolachas/Biscoitos",
       preco: 3.15,
