76a4c4f92670e930edaf58d4de27544d1509a150
JustG <guimachado94@gmail.com>
2023-01-24 17:15:47 -0300

    buttons

--- src/components/produto.jsx
+++ src/components/produto.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import no_image from "./img/no-image.png"
-import { BsPlus, BsDash } from "react-icons/bs";
+
 
 import "./style/produto.css"
 
@@ -14,8 +14,8 @@ const Produto = ({produto}) => {
                 <p>{produto.nome}</p>
 
                 <div className="compra">
-                    <button><BsDash/></button>
-                    <button><BsPlus/></button>
+                    <button></button>
+                    <button></button>
                 </div>
             </div>
             
