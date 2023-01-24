629e37f50b54653f55ba8e5e93e4ce766bfa5195
JustG <guimachado94@gmail.com>
2023-01-24 17:09:56 -0300

    botoes compra

--- src/components/produto.jsx
+++ src/components/produto.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import no_image from "./img/no-image.png"
+import { BsPlus, BsDash } from "react-icons/bs";
 
 import "./style/produto.css"
 
@@ -11,6 +12,11 @@ const Produto = ({produto}) => {
                 <img src={produto.img || no_image} alt={produto.nome} />
 
                 <p>{produto.nome}</p>
+
+                <div className="compra">
+                    <button><BsDash/></button>
+                    <button><BsPlus/></button>
+                </div>
             </div>
             
         </>
